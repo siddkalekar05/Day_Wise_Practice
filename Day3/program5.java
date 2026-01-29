f78041c51bb843487bf7bcba6023d0ae7137d140
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-29 22:35:58 +0530

    Add files via upload

---
+++ Day3/program5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Demo1 {
+    static void reverse(String s){
+        StringBuffer sb = new StringBuffer(s);
+        sb.reverse();
+        String reverse = new String(sb);
+        System.out.println(reverse);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String s = sc.nextLine();
+        reverse(s);
+    } 
+}
+
