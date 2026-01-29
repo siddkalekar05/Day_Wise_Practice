25ba242b8abc6e0cc81271a31b758d4a1124edf4
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-29 22:33:41 +0530

    Add files via upload

---
+++ Day3/program3.java
@@ -0,0 +1,18 @@
+class Parent {
+    int sum(int a , int b ){
+        return a+b;
+    }   
+}
+class Child extends Parent {
+    int sum(int a,int b,int c){
+        return a+b+c; 
+    }    
+}
+class Demo1 {
+    public static void main(String[] args) {
+        Child c = new Child();
+        System.out.println(c.sum(10,12, 14));
+        System.out.println(c.sum(12,13));
+    } 
+}
+
