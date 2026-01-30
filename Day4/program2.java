123c6cf8462e6f99e57eb8933197437d080156ce
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-30 22:20:46 +0530

    Add files via upload

---
+++ Day4/program2.java
@@ -0,0 +1,22 @@
+class Student {
+    String name;
+    int roll_no;
+    double cgpa;
+
+    Student(String name , int roll_no,double cgpa){
+        this.name = name;
+        this.roll_no = roll_no;
+        this.cgpa = cgpa;
+    }
+    void display(){
+        System.out.println("Student_name : "+name);
+        System.out.println("Roll_no : "+roll_no);
+        System.out.println("CGPA: "+cgpa);
+    }
+}
+class Client2 {
+    public static void main(String[] args) {
+        Student s = new Student("Siddhesh", 20, 8.78);
+        s.display();
+    }   
+}
