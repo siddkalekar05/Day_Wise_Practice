935ccc0139ec338c067a2ae478880c657d9aa492
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-01 22:44:56 +0530

    Update program6.java

--- Day6/program6.java
+++ Day6/program6.java
@@ -1,10 +1,16 @@
- class Praent{
+/*/*
+Problem:
+Demonstrate constructor chaining using super().
+ */
+
+class Praent{
         Praent(){
                 System.out.println("Parent Constructor");
         }
 }
 class Child extends Praent{
         Child(){
+                super();
                 System.out.println("Child Constructor");
         }
 
