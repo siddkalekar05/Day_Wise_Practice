28fdb4d958faff78068b1492bdac7d1b9157405e
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-01 22:30:22 +0530

    Add files via upload

---
+++ Day6/program2.java
@@ -0,0 +1,13 @@
+class Demo1 {
+        Demo1(){
+                System.out.println("Constructor is calleed");
+        }
+        Demo1(int a){
+                System.out.println("Parameterized constructor having paramter as a interger value :  "+a);
+        }
+        public static void main(String[] args) {
+                Demo1 obj1 = new Demo1();
+                Demo1 obj2 = new Demo1(12);
+        }
+}
+
