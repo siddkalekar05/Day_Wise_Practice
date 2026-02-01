8a68abc0b3f8815a5ffd541d21c09e921813736f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-01 22:34:07 +0530

    Add files via upload

---
+++ Day6/program5.java
@@ -0,0 +1,24 @@
+class ConstructorChaining {
+        int a;
+        int b;
+        ConstructorChaining(){
+                this(12,13);
+                System.out.println("Default Constructor called");
+        }
+        ConstructorChaining(int a, int b){
+                this.a = a;
+                this.b = b;
+                System.out.println("Parameterized Consructor");
+        }
+        
+        void display(){
+                System.out.println("a : "+a);
+                System.out.println("b : "+b);
+        }
+      
+        public static void main(String[] args) {
+                ConstructorChaining c1 = new ConstructorChaining();
+                c1.display();
+        }
+}
+
