048b5b3bfab5e9115994ee831d99c677c14090cf
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-02 22:50:32 +0530

    Add files via upload

---
+++ Day7/program5.java
@@ -0,0 +1,15 @@
+class Parent5 {
+    Parent5() {
+        System.out.println("Parent constructor");
+    }
+}
+
+class Child5 extends Parent5 {
+    Child5() {
+        System.out.println("Child constructor");
+    }
+
+    public static void main(String[] args) {
+        Child5 c = new Child5();
+    }
+}
