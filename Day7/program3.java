1d3ac2812fbd72102ca98781ff20b3186e590607
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-02 22:50:11 +0530

    Add files via upload

---
+++ Day7/program3.java
@@ -0,0 +1,17 @@
+class Parent2 {
+    void show() {
+        System.out.println("Parent show method");
+    }
+}
+
+class Child3 extends Parent2 {
+    @Override
+    void show() {
+        System.out.println("Child show method");
+    }
+
+    public static void main(String[] args) {
+        Parent2 p = new Child3();
+        p.show();  // calls Child's method
+    }
+}
