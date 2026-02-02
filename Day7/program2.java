44fdc7e66406cc0c7d8f93f7ef62055b52e0d9c0
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-02 22:49:34 +0530

    Add files via upload

---
+++ Day7/program2.java
@@ -0,0 +1,21 @@
+class GrandParent1 {
+        void display(){
+                System.out.println("GrandParent class Method");
+        } 
+}
+class Parent1 extends GrandParent1 {
+        void show(){
+                System.out.println("Parent class Method");
+        }
+}
+class Child1 extends Parent1{
+        void show1(){
+                System.out.println("Child class Method");
+        }
+        public static void main(String[] args) {
+                Child1 c = new Child1();
+                c.display();;
+                c.show();
+                c.show1();   
+        }
+}
