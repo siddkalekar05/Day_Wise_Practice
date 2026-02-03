517001c77e0492488be03df4705c3772714d8adc
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-03 22:37:24 +0530

    Add files via upload

---
+++ Day8/program1.java
@@ -0,0 +1,21 @@
+class MethodOverloadingDemo {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        MethodOverloadingDemo obj = new MethodOverloadingDemo();
+        System.out.println(obj.add(2, 3));
+        System.out.println(obj.add(1, 2, 3));
+        System.out.println(obj.add(2.5, 3.5));
+    }
+}
