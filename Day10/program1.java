a72e841647a19076eaae394ed5b022a1d105d5c3
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-04 22:41:49 +0530

    Update program1.java

--- Day10/program1.java
+++ Day10/program1.java
@@ -1,3 +1,19 @@
+/*
+Problem:
+Demonstrate abstraction using an abstract class.
+
+Concept:
+Abstract Class
+
+Explanation:
+- Abstract class can have abstract and non-abstract methods.
+- Object of abstract class cannot be created.
+- Used to hide implementation details.
+
+OOP Concept:
+Abstraction
+*/
+
 abstract class Vehicle {
     abstract void start();
 
