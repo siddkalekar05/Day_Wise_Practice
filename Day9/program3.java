69b60c84bc6563404a82ab8f364127572e2f372d
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-05 22:52:15 +0530

    Update program3.java

--- Day9/program3.java
+++ Day9/program3.java
@@ -1,6 +1,11 @@
+
+
+import java.util.*;
 class CountVowelsConsonants {
     public static void main(String[] args) {
-        String str = "Hello World";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+        String str = sc.nextLine;
         str = str.toLowerCase();
 
         int vowels = 0, consonants = 0;
