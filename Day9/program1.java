4fe572451c1c797fa5661f866a117aa48c51bdcb
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-05 22:49:11 +0530

    Update program1.java

--- Day9/program1.java
+++ Day9/program1.java
@@ -1,3 +1,21 @@
+/*
+Problem:
+Reverse a given string using StringBuffer.
+
+Concept:
+String manipulation using StringBuffer
+
+Explanation:
+- StringBuffer is mutable.
+- reverse() method is synchronized (thread-safe).
+- Suitable for multi-threaded environments.
+
+Why StringBuffer:
+- String is immutable.
+- StringBuilder is not thread-safe.
+- StringBuffer provides thread safety.
+	*/
+
 import java.util.*;
 class ReverseString {
     public static void main(String[] args) {
