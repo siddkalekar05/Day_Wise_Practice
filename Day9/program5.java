279bcda900d852b9559bdad41c40c9555ae7c46f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-05 22:47:42 +0530

    Add files via upload

---
+++ Day9/program5.java
@@ -0,0 +1,9 @@
+class EqualsVsDoubleEqual {
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+
+        System.out.println("Using == : " + (s1 == s2));
+        System.out.println("Using equals(): " + s1.equals(s2));
+    }
+}
