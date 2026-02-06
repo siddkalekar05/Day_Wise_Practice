1c90da738ec0cb63b48954e7362b7605502037b3
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-06 21:32:49 +0530

    Add files via upload

---
+++ Day11/program1.java
@@ -0,0 +1,14 @@
+interface Vehicle {
+    void start();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car starts with key");
+    }
+
+    public static void main(String[] args) {
+        Vehicle v = new Car();
+        v.start();
+    }
+}
