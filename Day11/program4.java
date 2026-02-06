720271aca11d7e5cb2969b03afee36dd8662e032
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-06 21:35:16 +0530

    Add files via upload

---
+++ Day11/program4.java
@@ -0,0 +1,11 @@
+interface Calculator {
+    static void info() {
+        System.out.println("Calculator interface");
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Calculator.info();
+    }
+}
