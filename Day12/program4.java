c9c5b22b68fb351f006f4ff0efb13bb0151233da
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-07 22:31:28 +0530

    Add files via upload

---
+++ Day12/program4.java
@@ -0,0 +1,23 @@
+class LowBatteryException extends Exception {
+    LowBatteryException(String message) {
+        super(message);
+    }
+}
+
+class ElectricScooter {
+
+    static void startScooter(int batteryLevel) throws LowBatteryException {
+        if (batteryLevel < 20) {
+            throw new LowBatteryException("Battery too low!");
+        }
+        System.out.println("Scooter started");
+    }
+
+    public static void main(String[] args) {
+        try {
+            startScooter(10);
+        } catch (LowBatteryException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
