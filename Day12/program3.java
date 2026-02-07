a7bf4abc3b0334859215657bc6171bcd5a515e28
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-07 22:31:08 +0530

    Add files via upload

---
+++ Day12/program3.java
@@ -0,0 +1,17 @@
+class SmartHomeSensor {
+
+    static void readTemperature() {
+        try {
+            int temperature = 25 / 0;
+            System.out.println(temperature);
+        } catch (ArithmeticException e) {
+            System.out.println("Sensor malfunction detected!");
+        } finally {
+            System.out.println("Sensor check complete.");
+        }
+    }
+
+    public static void main(String[] args) {
+        readTemperature();
+    }
+}
