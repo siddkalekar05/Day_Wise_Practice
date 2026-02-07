75a4f462fade943a41fa3084d316e6d2abf905ed
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-07 22:39:24 +0530

    Update program6.java

--- Day12/program6.java
+++ Day12/program6.java
@@ -1,1 +1,20 @@
 
+
+class OnlinePaymentValidation {
+
+    static void processPayment(double amount) throws Exception {
+        if (amount <= 0) {
+            throw new Exception("Invalid payment amount!");
+        }
+        System.out.println("Payment processed: ₹" + amount);
+    }
+
+    public static void main(String[] args) {
+        try {
+            processPayment(-500);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
