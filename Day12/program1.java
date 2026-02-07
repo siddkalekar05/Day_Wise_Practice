121ceb8e172ac0d3b77cdf72b001f8e2fca10861
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-07 22:30:26 +0530

    Add files via upload

---
+++ Day12/program1.java
@@ -0,0 +1,17 @@
+class TicketBooking {
+
+    static void bookTickets(int availableSeats, int requestedSeats) throws Exception {
+        if (requestedSeats > availableSeats) {
+            throw new Exception("Not enough seats available!");
+        }
+        System.out.println("Booking successful");
+    }
+
+    public static void main(String[] args) {
+        try {
+            bookTickets(5, 10);
+        } catch (Exception e) {
+            System.out.println("Booking failed: " + e.getMessage());
+        }
+    }
+}
