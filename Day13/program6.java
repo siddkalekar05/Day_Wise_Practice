7ae2f68a0a0d19f4f5fd5a3258bf4c7ff8a3561f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-08 22:22:16 +0530

    Add files via upload

---
+++ Day13/program6.java
@@ -0,0 +1,23 @@
+class PaymentGateway {
+    final void transactionID() {
+        System.out.println("Transaction ID generated");
+    }
+}
+
+class OnlineGateway extends PaymentGateway {
+    //  This would cause compile error
+    // void transactionID() { }
+    
+    void process() {
+        System.out.println("Processing payment...");
+    }
+}
+
+class DemoFinalMethod {
+    public static void main(String[] args) {
+        OnlineGateway g = new OnlineGateway();
+        g.transactionID();
+        g.process();
+    }
+}
+
