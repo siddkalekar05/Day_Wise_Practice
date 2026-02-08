d4781d6a5e133a0e686ceadeb414bea280d6f734
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-08 22:21:46 +0530

    Add files via upload

---
+++ program5.java
@@ -0,0 +1,21 @@
+class Count3 {
+    static int a = 5;
+    //static final  int a = 10;  this occures error bcz final keyword prevents changes 
+    Count3(){
+        a++;
+    }
+    void display(){
+        System.out.println("Value inc a : "+ a);
+    }
+}
+class Client5{
+    public static void main(String[] args) {
+       Count3 c;
+       c = new Count3();
+       c = new Count3();
+       c = new Count3();
+       c = new Count3();
+       c.display();  
+    }
+}
+
