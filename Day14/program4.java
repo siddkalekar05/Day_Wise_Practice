61dee593ad3da80ccea2357d977faa26aa28699f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-09 22:22:28 +0530

    Update program4.java

--- Day14/program4.java
+++ Day14/program4.java
@@ -1,3 +1,20 @@
+/*
+Day 14 — Collections (List)
+
+Program:
+Sorting ArrayList
+
+Concept:
+Collections.sort()
+
+Explanation:
+- Uses TimSort internally.
+- Sorts list in ascending order.
+
+Time Complexity:
+O(n log n)
+*/
+
 import java.util.*;
 class CollectionDemo4 {
     public static void main(String[] args) {
