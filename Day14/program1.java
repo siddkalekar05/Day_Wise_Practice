3754ebc8b38e473326a4246a6a7e3a9bcb1d4114
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-09 22:20:18 +0530

    Update program1.java

--- Day14/program1.java
+++ Day14/program1.java
@@ -1,3 +1,22 @@
+/*
+Day 14 — Collections (List)
+
+Program:
+ArrayList add and display example
+
+Concept:
+ArrayList
+
+Explanation:
+- ArrayList is a dynamic array.
+- Elements can be added using add().
+- Size grows automatically.
+
+Time Complexity:
+Add → O(1)
+Access → O(1)
+*/
+
 import java.util.*;
 class CollectionDemo1 {
     public static void main(String[] args) {
