d1456671c99411ddc413a931431739745dc0cdeb
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-09 22:14:01 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CollectionDemo1 {
+    public static void main(String[] args) {
+        List<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+
+        System.out.println(al);
+    }
+    
+}
