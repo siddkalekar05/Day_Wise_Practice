6199903ce4854be60c5b15d3290fe440ab1e5652
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-09 22:18:06 +0530

    Add files via upload

---
+++ Day14/program4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class CollectionDemo4 {
+    public static void main(String[] args) {
+        List<Integer> al = new ArrayList<>();
+        al.add(11);
+        al.add(59);
+        al.add(56);
+        al.add(44);
+        System.out.println(al);
+        Collections.sort(al);
+        System.out.println(al);
+
+
+    }
+    
+}
