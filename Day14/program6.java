fd31f6a1981142a79f9a373fb9abd6e4772e98ae
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-09 22:18:49 +0530

    Add files via upload

---
+++ Day14/program6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.LinkedList;
+class CollectionDemo6 {
+    public static void main(String[] args) {
+        List<String> al = new LinkedList<>();
+        al.add("Sidd");
+        al.add("Kalekar");
+        al.add("Oneplus11r");
+        al.add("Asus Tuf15");
+
+        System.out.println(al);
+    } 
+}
