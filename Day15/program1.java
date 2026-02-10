f737a6ee30a3c4b04cd13c86b9b6ba766be5baf5
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-10 22:25:15 +0530

    Add files via upload

---
+++ Day15/program1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class CollectionDemo1{
+    public static void main(String[] args) {
+       HashSet<Integer> st = new HashSet<>();
+       st.add(30);
+       st.add(10);
+       st.add(10);
+       st.add(20);
+
+       System.out.println(st);
+    }
+}
