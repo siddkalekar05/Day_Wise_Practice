b34d723d0ebbb8cb1174387aa33b49d6b320cddd
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-10 22:25:50 +0530

    Add files via upload

---
+++ Day15/program3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class CollectionDemo3{
+    public static void main(String[] args) {
+      TreeSet<String> ls = new TreeSet<>();
+       ls.add("Pune");
+       ls.add("Nashik");
+       ls.add("Mumbai");
+       ls.add("Nagpur");
+       System.out.println(ls);
+
+       TreeSet<Integer> ts = new TreeSet<>();
+       ts.add(56);
+       ts.add(45);
+       ts.add(18);
+       ts.add(7);
+       ts.add(1);
+       System.out.println(ts);
+    }
+}
