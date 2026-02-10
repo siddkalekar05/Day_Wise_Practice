9a9d7fa0794c98a1b0ba98079be5117253ba6eba
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-10 22:26:57 +0530

    Add files via upload

---
+++ Day15/program5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class CollectionDemo5{
+    public static void main(String[] args) {
+      HashMap<Integer , String> hm = new HashMap<>();
+      hm.put(1, " Siddhesh");
+      hm.put(2, " Santosh");
+      hm.put(3, " Manisha");
+      hm.put(4, " Piyush"); 
+
+      System.out.println(hm);
+
+     for (Map.Entry<Integer, String> entry : hm.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+         }
+    }
+}
