5a7a87650154df4d95630d07ce2deb5a81691429
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-11 22:49:00 +0530

    Add files via upload

---
+++ Day16/program5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class PriQueDemo{
+	public static void main(String[] args){
+		PriorityQueue<Integer> p = new PriorityQueue<>();
+		p.add(45);
+		p.add(18);
+		p.add(7);
+		System.out.println(p);
+	}
+}
