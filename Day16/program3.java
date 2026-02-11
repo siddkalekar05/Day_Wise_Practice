944934da66a8a57565a4fcc4e09ade8c11f152ea
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-11 22:47:54 +0530

    Add files via upload

---
+++ Day16/program3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class StackDemo1{
+	public static void main(String[] args){
+		Stack<Integer> s = new Stack<>();
+		s.push(5);
+		s.push(10);
+		s.push(15);
+		s.push(20);
+		System.out.println(s);
+	}
+}
