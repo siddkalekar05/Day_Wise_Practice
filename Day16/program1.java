016ebbc049843765945e92e2e800ca642d352d35
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-11 22:44:54 +0530

    Add files via upload

---
+++ Day16/program1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class QueueDemo{
+	public static void main(String[] args){
+		Queue<String> q = new LinkedList<>();
+		q.add("Siddhesh");
+		q.add("Santosh");
+		q.add("Kalekar");
+
+		System.out.println(q);
+	}
+}
