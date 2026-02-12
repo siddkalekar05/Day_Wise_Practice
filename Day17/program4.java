ca81ba7360954db8786853eb5289cf16f84e6859
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-12 23:11:19 +0530

    Add files via upload

---
+++ Day17/program4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class DequeDemo4{
+	public static void main(String[] args){
+		Deque<Integer> q = new ArrayDeque<>();
+		q.offer(10);
+		q.offer(20);
+		q.offer(30);
+		q.offer(40);
+		System.out.println(q);
+	}
+}
