c7f5a94f747c583e66937cb5eb8f2a4355351e61
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-12 23:09:30 +0530

    Add files via upload

---
+++ Day17/program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class DequeDemo1{
+	public static void main(String[] args){
+		Deque<Integer> qd = new ArrayDeque<>();
+		
+		qd.addFirst(10);
+		qd.addLast(20);
+		qd.addFirst(30);
+		qd.addLast(40);
+
+		System.out.println(qd);
+	}
+}
