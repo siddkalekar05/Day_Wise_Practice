fe6e7122b5be3e961ab202cbf8c2a425701ab342
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-13 22:26:16 +0530

    Add files via upload

---
+++ Day18/program3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class IteratorDemo3 implements Comparable<IteratorDemo3>{
+	int marks;
+	IteratorDemo3(int marks){
+		this.marks = marks;
+	}
+	public int compareTo(IteratorDemo3 i){
+		return this.marks - i.marks;
+	}
+	public String toString(){
+		return ""+marks;
+	}
+}
+class Example{
+	public static void main(String[] args){
+		List<IteratorDemo3> ll = new ArrayList<>();
+		ll.add(new IteratorDemo3(70));
+		ll.add(new IteratorDemo3(50));
+		ll.add(new IteratorDemo3(90));
+		System.out.println(ll);
+
+		Collections.sort(ll);
+
+		System.out.println(ll);
+	}
+}
