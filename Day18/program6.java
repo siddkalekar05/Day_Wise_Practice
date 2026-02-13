49d76f562e1c4acdafcb8ebf8692d8e741fd229e
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-13 22:37:20 +0530

    Update program6.java

--- Day18/program6.java
+++ Day18/program6.java
@@ -4,14 +4,13 @@ Custom sorting using comparator.
 */
 
 import java.util.*;
-
 class IteratorDemo6 {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("Java");
         list.add("Programming");
         list.add("AI");
-
+        System.out.println(list);
         Collections.sort(list, (a, b) -> a.length() - b.length());
 
         System.out.println(list);
