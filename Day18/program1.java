dd2b8b3af4840f3529507695df0d39191ee577bf
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-13 22:25:20 +0530

    Add files via upload

---
+++ Day18/program1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class IteratorDemo1{
+	public static void main(String[] args){
+		List<Integer> ll = new ArrayList<>();
+		ll.add(1);
+		ll.add(2);
+		ll.add(3);
+		ll.add(4);
+		System.out.println(ll);
+
+		Iterator itr = ll.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
+	}
+}
