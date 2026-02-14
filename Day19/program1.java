e637be88450671b71c8cc2ea9bcd3b62cce30b9a
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-14 22:47:08 +0530

    Add files via upload

---
+++ Day19/program1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class FileDemo1{
+	public static void main(String[] args)throws IOException{
+		File f = new File("Sample.txt");
+
+		if(f.createNewFile()){
+			System.out.println("File created");
+		}else{
+			System.out.println("File Already Exist");
+		}
+	}
+}
