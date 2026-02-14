d16061e09d95b456a65df4d554e0be1e2240e098
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-14 22:57:16 +0530

    Update program6.java

--- Day19/program6.java
+++ Day19/program6.java
@@ -2,9 +2,7 @@
 Concept:
 Simple log writer using FileWriter.
 */
-
 import java.io.*;
-
 class FileDemo6 {
     public static void main(String[] args) throws Exception {
         FileWriter fw = new FileWriter("log.txt", true);
