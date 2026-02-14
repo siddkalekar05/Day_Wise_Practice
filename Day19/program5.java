78208e6497cabe6ecdd4b69311113dfbc3304643
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-14 22:52:18 +0530

    Add files via upload

---
+++ Day19/program5.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class FileDemo5{
+	public static void main(String[] args)throws IOException{
+		PrintWriter pw = new PrintWriter("Sample.txt");
+		pw.write("Line 1");
+		pw.write("Line2");
+		pw.close();
+	}
+}
