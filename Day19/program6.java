10dfc962d72621a27c05c9ecdebea6032d465e76
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-14 22:52:38 +0530

    Add files via upload

---
+++ Day19/program6.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class FileDemo6 {
+    public static void main(String[] args) throws Exception {
+        FileWriter fw = new FileWriter("log.txt", true);
+
+        fw.write("Application started\n");
+        fw.close();
+    }
+}
