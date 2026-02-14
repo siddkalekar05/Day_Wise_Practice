258e164c5166623411c3052dc42e3abec3783c56
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-14 22:51:28 +0530

    Add files via upload

---
+++ Day19/program3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class FileDemo3{
+	public static void main(String[] args)throws IOException{
+		FileReader fr = new FileReader("Sample.txt");
+		int ch;
+		while((ch=fr.read())!=-1){
+			System.out.print((char)ch);
+		}
+		System.out.println();
+		fr.close();
+	}
+}
