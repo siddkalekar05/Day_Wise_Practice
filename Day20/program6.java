e51aa1b77b5d77bf7f15cf526475fc74bd66400a
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-15 22:37:01 +0530

    Add files via upload

---
+++ Day20/program6.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class SerializeUser {
+    public static void main(String[] args) throws Exception {
+        User u = new User("admin", "1234");
+
+        ObjectOutputStream out =
+                new ObjectOutputStream(new FileOutputStream("user.ser"));
+
+        out.writeObject(u);
+        out.close();
+    }
+}
