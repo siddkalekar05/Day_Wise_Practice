be618c80e304786558c1e0d2809b10a6eee9539e
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-16 22:50:38 +0530

    Add files via upload

---
+++ Day21/program2.java
@@ -0,0 +1,12 @@
+class ThreadDemo2 implements Runnable{ 
+        public void run(){
+                System.out.println("Thread Create : "+Thread.currentThread().getName());
+        }
+}
+class Client2{
+        public static void main(String[] args){
+                Thread t = new Thread(new ThreadDemo2());
+                t.start();
+                System.out.println("Main thread : "+Thread.currentThread().getName());
+        }
+}
