c3c14ae783b944d120a63f36837b042ac471a2a2
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-16 22:54:17 +0530

    Update program2.java

--- Day21/program2.java
+++ Day21/program2.java
@@ -1,3 +1,10 @@
+/*
+Day 21 — Multithreading
+
+Concept:
+Creating thread using Runnable Interface.
+*/
+
 class ThreadDemo2 implements Runnable{ 
         public void run(){
                 System.out.println("Thread Create : "+Thread.currentThread().getName());
