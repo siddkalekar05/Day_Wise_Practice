aae673ffd655bf8d95e8f002aad603f8513a8cda
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-17 21:42:18 +0530

    Add files via upload

---
+++ Day22/program6.java
@@ -0,0 +1,17 @@
+class Message {
+    synchronized void print(String msg) {
+        System.out.println(msg);
+    }
+}
+
+class SyncDemo2 {
+    public static void main(String[] args) {
+        Message m = new Message();
+
+        Thread t1 = new Thread(() -> m.print("Hello"));
+        Thread t2 = new Thread(() -> m.print("World"));
+
+        t1.start();
+        t2.start();
+    }
+}
