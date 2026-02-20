7a13e4cb7ffc4711a3fded5dce428edf9def3d05
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-20 22:42:00 +0530

    Add files via upload

---
+++ Day25/program1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class StreamDemo1{
+	public static void main(String[] args){
+		List<Integer> l = new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		System.out.println(l);
+		l.stream().forEach(System.out::println);
+	}
+}
