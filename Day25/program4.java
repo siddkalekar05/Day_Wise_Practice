2d378716aa92812f5d4f4f69c7e06a63a6cce29c
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-20 22:43:13 +0530

    Add files via upload

---
+++ Day25/program4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StreamDemo4{
+        public static void main(String[] args){
+                List<Integer> l = new ArrayList<>();
+                l.add(5);
+                l.add(8);
+                l.add(3);
+                l.add(4);
+                System.out.println(l);
+                long count = l.stream().filter(n->n>5).count();
+		System.out.println(count);
+        }
+}
