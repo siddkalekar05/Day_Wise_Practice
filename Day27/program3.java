519519f8af83291335381cb8bd06b1af16fdef8b
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-22 22:48:00 +0530

    Update program3.java

--- Day27/program3.java
+++ Day27/program3.java
@@ -1,3 +1,22 @@
+/*
+Day 27 — Exception Handling
+
+Program:
+Using finally block
+
+Concept:
+finally always executes
+
+Explanation:
+- try block contains risky code.
+- catch handles exception if occurs.
+- finally block executes whether exception occurs or not.
+- Commonly used for closing resources (files, DB connections).
+
+Learning Outcome:
+Understanding guaranteed execution using finally.
+*/
+
 class EXCPDemo3{
 	public static void main(String[] args){
 		int a = 7;
