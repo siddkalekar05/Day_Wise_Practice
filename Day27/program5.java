f1ad39a92c6ce2fc88af82606a946dbd9a1c4ac8
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-22 22:45:57 +0530

    Add files via upload

---
+++ Day27/program5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class EXCPDemo5 {
+
+    static void readFile() throws IOException {
+        FileReader fr = new FileReader("abc.txt");
+    }
+
+    public static void main(String[] args) {
+        try {
+            readFile();
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
