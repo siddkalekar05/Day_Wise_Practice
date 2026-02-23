54065880628e63cb653616b1976a598a749d3e90
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-23 20:48:22 +0530

    Add files via upload

---
+++ Day28/program4.java
@@ -0,0 +1,11 @@
+class ExcpDemo4 {
+    public static void main(String[] args) {
+
+        try {
+            String s = null;
+            System.out.println(s.length());
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println("Handled multiple exception types");
+        }
+    }
+}
