b44c56b4a61ebcad5a866511e394131c029109c0
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-23 20:45:57 +0530

    Add files via upload

---
+++ Day28/program2.java
@@ -0,0 +1,22 @@
+class ExcpDemo2 {
+
+    static void method3() {
+        int a = 10 / 0;
+    }
+
+    static void method2() {
+        method3();
+    }
+
+    static void method1() {
+        method2();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("Handled in main");
+        }
+    }
+}
