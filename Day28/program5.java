2511f9e281205b5d99ed4d83bf78a3805ea1a28b
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-23 20:49:10 +0530

    Add files via upload

---
+++ Day28/program5.java
@@ -0,0 +1,19 @@
+class InvalidAmountException extends RuntimeException {
+    InvalidAmountException(String msg) {
+        super(msg);
+    }
+}
+
+class Example5 {
+
+    static void withdraw(int amount) {
+        if (amount > 1000)
+            throw new InvalidAmountException("Limit exceeded");
+
+        System.out.println("Withdraw successful");
+    }
+
+    public static void main(String[] args) {
+        withdraw(2000);
+    }
+}
