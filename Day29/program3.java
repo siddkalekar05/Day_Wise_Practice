1480a82b541641507fc86769465f5b26ba26ffbf
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-24 21:26:54 +0530

    Add files via upload

---
+++ Day29/program3.java
@@ -0,0 +1,19 @@
+class ExcpDemo2 {
+
+    static void process() {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Logging error...");
+            throw e; // rethrow
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            process();
+        } catch (Exception e) {
+            System.out.println("Handled in main");
+        }
+    }
+}
