d8d7ee6d5c7fd8cb022559842409eeb8898b71aa
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-24 21:26:38 +0530

    Add files via upload

---
+++ Day29/program2.java
@@ -0,0 +1,9 @@
+class ExcpDemo1Correct {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
