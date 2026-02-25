2d3a32ed1875d54af41d70221235c755fad1b1c5
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-25 20:32:10 +0530

    Create Program4.java

---
+++ Day30/Program4.java
@@ -0,0 +1,17 @@
+class ExcpDemo4 {
+
+    static int test() {
+
+        try {
+            return 10;
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int result = test();
+        System.out.println("Returned: " + result);
+    }
+}
