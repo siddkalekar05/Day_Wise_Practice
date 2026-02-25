37387c1cca28eeb0f8e737a9cc36d18cfa3def0e
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-02-25 20:27:42 +0530

    Update Program1.java

--- Day30/Program1.java
+++ Day30/Program1.java
@@ -1,1 +1,27 @@
+class ExcpDemo1 {
 
+    static void lowLevel() {
+        throw new ArithmeticException("Division failed");
+    }
+
+    static void highLevel() {
+        try {
+            lowLevel();
+        } catch (ArithmeticException e) {
+
+            // Wrap and preserve original cause
+            throw new RuntimeException("High level failure", e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            highLevel();
+        } catch (RuntimeException e) {
+
+            System.out.println("Message: " + e.getMessage());
+            System.out.println("Root Cause: " + e.getCause());
+        }
+    }
+}
